d35a82f58a43538778989b8ccab6b708bc2a80dc
dmitriysemenko <dm123semenko@gmail.com>
2021-09-29 01:31:25 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
 
 class App extends Component {
     render() {
         return (
             <>
-                <Navbar /> <UserItem />
+                <Navbar /> <Users />
             </>
         );
     }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,31 +1,20 @@
 import React, { Component } from "react";
 
 export class UserItem extends Component {
-    constructor() {
-        super();
-        this.state = {
-            id: 1,
-            login: "mojombo",
-            avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-            html_url: "https://github.com/mojombo",
-        };
-    }
-
     render() {
+        const { login, avatar_url, html_url } = this.props.user;
+
         return (
             <div className="card text-center">
                 <img
-                    src={this.state.avatar_url}
+                    src={avatar_url}
                     alt="Avatar"
                     className="round-img"
                     style={{ width: "70px" }}
                 />
-                <h3>{this.state.login}</h3>
-                <a
-                    href={this.state.html_url}
-                    className="btn btn-dark btn-sm my-1"
-                >
-                    GitHub Profile
+                <h3>{login}</h3>
+                <a href={html_url} className="btn btn-primary btn-sm my-1">
+                    Profile
                 </a>
             </div>
         );
