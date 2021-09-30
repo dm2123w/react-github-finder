0000f40347301a1ce2f5a72b2eb494dd7746a416
dmitriysemenko <dm123semenko@gmail.com>
2021-09-30 16:30:13 +0500

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 
 class App extends Component {
     state = {
@@ -12,15 +13,26 @@ class App extends Component {
 
     async componentDidMount() {
         this.setState({ loading: true });
-        const res = await axios.get("https://api.github.com/users");
+        const res = await axios.get(
+            `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
         this.setState({ users: res.data, loading: false });
     }
+
+    searchUsers = async (text) => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        this.setState({ users: res.data.items, loading: false });
+    };
 
     render() {
         return (
             <>
                 <Navbar />{" "}
                 <div className="container">
+                    <Search searchUsers={this.searchUsers} />
                     <Users
                         users={this.state.users}
                         loading={this.state.loading}
