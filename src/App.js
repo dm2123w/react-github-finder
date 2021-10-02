54fb86f60d4bcf970415605572944a856b56280a
dmitriysemenko <dm123semenko@gmail.com>
2021-10-02 20:11:30 +0500

    update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import { Alert } from "./components/layout/Alert";
 
 class App extends Component {
     state = {
         users: [],
         loading: false,
+        alert: null,
     };
 
     searchUsers = async (text) => {
@@ -23,15 +25,21 @@ class App extends Component {
         this.setState({ users: [], loading: false });
     };
 
+    setAlert = (message, type) => {
+        this.setState({ alert: { message, type } });
+    };
+
     render() {
         return (
             <>
                 <Navbar />{" "}
                 <div className="container">
+                    <Alert alert={this.state.alert} />
                     <Search
                         searchUsers={this.searchUsers}
                         clearUsers={this.clearUsers}
                         showClear={this.state.users.length > 0 ? true : false}
+                        setAlert={this.setAlert}
                     />
                     <Users
                         users={this.state.users}
