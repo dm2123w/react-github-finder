831c2e45f8fe2005bdef3bb668f1d89955dcad19
dmitriysemenko <dm123semenko@gmail.com>
2021-10-03 14:51:40 +0500

    class-based app done

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     state = {
         users: [],
         user: {},
+        repos: [],
         loading: false,
         alert: null,
     };
@@ -33,6 +34,14 @@ class App extends Component {
         this.setState({ user: res.data, loading: false });
     };
 
+    getUserRepos = async (username) => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        this.setState({ repos: res.data, loading: false });
+    };
+
     clearUsers = () => {
         this.setState({ users: [], loading: false });
     };
@@ -43,7 +52,7 @@ class App extends Component {
     };
 
     render() {
-        const { users, user, loading } = this.state;
+        const { users, user, repos, loading } = this.state;
 
         return (
             <Router>
@@ -80,6 +89,8 @@ class App extends Component {
                                     <User
                                         {...props}
                                         getUser={this.getUser}
+                                        getUserRepos={this.getUserRepos}
+                                        repos={repos}
                                         user={user}
                                         loading={loading}
                                     />
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import { Spinner } from "../layout/Spinner";
+import { Repos } from "../repos/Repos";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 export class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
+        this.props.getUserRepos(this.props.match.params.login);
     }
 
     static propTypes = {
         loading: PropTypes.bool,
         user: PropTypes.object.isRequired,
+        repos: PropTypes.array.isRequired,
         getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
     };
 
     render() {
@@ -31,7 +35,7 @@ export class User extends Component {
             hireable,
         } = this.props.user;
 
-        const { loading } = this.props;
+        const { loading, repos } = this.props;
 
         if (loading) return <Spinner />;
 
@@ -109,6 +113,7 @@ export class User extends Component {
                         Public Gists: {public_gists}
                     </div>
                 </div>
+                <Repos repos={repos} />
             </>
         );
     }
