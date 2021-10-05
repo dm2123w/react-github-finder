7b3141b42ce3e31095b6e40309ef8d78377f971b
dmitriysemenko <dm123semenko@gmail.com>
2021-10-05 13:27:14 +0500

    update

---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Search from "../users/Search";
+import Users from "../users/Users";
+
+export const Home = () => (
+    <>
+        <Search />
+        <Users />
+    </>
+);
